a1b70f423fa0117eb1386acedb2101c8f8fb3d54
imaginarysamuel <getsamuel@gmail.com>
2026-01-21 15:49:50 -0600

    Update list_script.js

--- list_script.js
+++ list_script.js
@@ -544,7 +544,7 @@ function printSingleCard(card) {
     
     setTimeout(() => {
       html2pdf().set(opt).from(printContent).save();
-    }, 200);
+    }, 500);
     
   } catch (error) {
     console.error('Error generating PDF:', error);
