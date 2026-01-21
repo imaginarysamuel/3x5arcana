cb789b810d2d322729a8f04f9a3a5b80d8c662db
imaginarysamuel <getsamuel@gmail.com>
2026-01-21 10:50:42 -0600

    Update spells_script.js

--- spells_script.js
+++ spells_script.js
@@ -79,6 +79,7 @@ function getCardInnerHTML(spell, spellId) {
     : "P";
 
   return `
+    ${getCardActionButtonsHTML()}
     <div class="card-header">
       <div class="card-favorite-title">
         <div class="favorite-icon" id="${spellId}-favorite-icon">●</div>
