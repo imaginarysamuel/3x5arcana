711b1a78d28c013043840194ae75ed51caba4f9f
imaginarysamuel <getsamuel@gmail.com>
2026-01-21 15:31:38 -0600

    Update list_script.js

--- list_script.js
+++ list_script.js
@@ -790,8 +790,8 @@ function buildPrintStyles() {
       
       .print-card-branding {
         position: absolute;
-        bottom: 0.2in;
-        right: 0.25in;
+        bottom: 0.1in;
+        right: 0.1in;
         font-size: 7pt;
         color: #7d7d7d;
         text-align: right;
